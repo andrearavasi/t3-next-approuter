5148e92401897f9ae39206cfe7026a05ef993fa6
Andrea <ravasi.andrea@gmail.com>
2024-04-16 23:49:42 +0200

    drizzle, force dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import Link from "next/link";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+export const dynamic = "force-dynamic";
+
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <p>ciaoo</p>
+      <div className="flex flex-wrap bg-slate-500 p-4 gap-4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+        <p>ciaoo</p>
+      </div>
     </main>
   );
 }
