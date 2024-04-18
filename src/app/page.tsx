e85262f29b3f3e037f2d58bca730764c2a02887f
Andrea <ravasi.andrea@gmail.com>
2024-04-19 00:08:53 +0200

    merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,4 @@
-<<<<<<< HEAD
-import { db } from "~/server/db";
 
-export const dynamic = "force-dynamic";
-
-export default async function HomePage() {
-
-  const posts = await db.query.posts.findMany();
-
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="flex flex-wrap bg-slate-500 p-4 gap-4">
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-        <p>ciaoo</p>
-      </div>
-    </main>
-=======
 import Link from "next/link";
 import { Button } from "~/components/ui/button";
 
@@ -33,6 +15,5 @@ export default async function HomePage() {
       </Button>
 
     </div>
->>>>>>> f3f756950012f1cff7ccb186354c8c97c27a8a6b
   );
 }
